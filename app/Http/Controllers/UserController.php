894077eb6c45da3670e3809c3c090fc6801ddfe2
FarhanMawaludin <farhanmawaludin7@gmail.com>
2025-03-03 11:26:33 +0700

    Praktikum 1 - $fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,13 +6,22 @@ use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
+
 class UserController extends Controller
 {
     public function index(){
-        $data=[ 
-            'nama' => 'Pelanggan Pertama'
+        // $data=[ 
+        //     'nama' => 'Pelanggan Pertama'
+        // ];
+        // UserModel::where('username','customer-1')->update($data);
+
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345'),
         ];
-        UserModel::where('username','customer-1')->update($data);
+        UserModel::create($data);
 
         $user=UserModel::all();
         return view('user',['data'=>$user]);
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -11,4 +11,10 @@ class UserModel extends Model
 
     protected $table = 'm_user';
     protected $primaryKey = 'id_user';
+
+    protected $fillable = [
+        'level_id',
+        'username',
+        'nama',
+    ];
 }
